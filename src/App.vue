7fe32260b4f9b6265c86f589cb240312d5a1e174
HHH917 <917388227@QQ.COM>
2019-08-03 10:30:31 +0800

    init my project

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,35 @@
 <template>
   <div class="app-container">
+    <!-- 顶部header 区域 -->
+    <mt-header fixed title="vue项目顶部"></mt-header>
+    <!-- 中间的路由区域 router-view -->
 
-    <!-- 顶部 Header 区域 -->
-    <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
-
-
-    <!-- 中间的 路由 router-view 区域 -->
-		<transition>
-			<router-view></router-view>
-		</transition>
-
-
-    <!-- 底部 Tabbar 区域 -->
-    <nav class="mui-bar mui-bar-tab">
-			<router-link class="mui-tab-item" to="/home">
+    <!-- 底部tabbar区域 -->
+	<nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/member">
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
 				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">会员</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/shopcar">
-				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
-					<span class="mui-badge">0</span>
-				</span>
-				<span class="mui-tab-label">购物车</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/search">
-				<span class="mui-icon mui-icon-search"></span>
-				<span class="mui-tab-label">搜索</span>
-			</router-link>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
 		</nav>
-
+    <h1>这是 APP 组件</h1>
   </div>
 </template>
-
 <script>
 </script>
-
-
-<style lang="scss" scoped>
-.app-container {
-  padding-top: 40px;
-  overflow-x: hidden;
-}
-
-.v-enter {
-  opacity: 0;
-  transform: translateX(100%);
-}
-
-.v-leave-to {
-  opacity: 0;
-  transform: translateX(-100%);
-  position: absolute;
-}
-
-.v-enter-active,
-.v-leave-active {
-  transition: all 0.5s ease;
-}
+<style lang='scss' scoped>
+	.app-container{
+		padding-top: 40px;
+	}
 </style>
