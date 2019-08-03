95b88d620e82a3bc12fd01a75e5b3c9648f81665
HHH917 <917388227@QQ.COM>
2019-08-03 11:27:26 +0800

    修改了app.vue 与main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 
 //导入mui 的样式
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 // 按需导入mint-ui 中的组件
 import { Header } from 'mint-ui'
 Vue.component(Header.name,Header)
